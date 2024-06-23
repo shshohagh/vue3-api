bf129f7c40998fe4c32cbc614bacc14b82eeee17
Sohag <sksitfirm@gmail.com>
2024-06-24 02:21:46 +0600

    add Product Rating

--- app/Http/Controllers/ReviewApiController.php
+++ app/Http/Controllers/ReviewApiController.php
@@ -3,8 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Review;
+use Illuminate\Support\Facades\Validator;
 
 class ReviewApiController extends Controller
 {
-    //
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            'product_id' => 'required|max:191',
+            'customer' => 'required|max:191',
+            'review' => 'required|max:191',
+            'star' => 'required|max:191',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages(),
+            ], 422);
+        }else{
+            $review = Review::create([
+                'product_id' => $request->input('product_id'),
+                'customer' => $request->input('customer'),
+                'review' => $request->input('review'),
+                'star' => $request->input('star'),
+            ]);
+
+            if($review){
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Review Added Successfully'
+                ], 200);
+            }else{
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'Somthing Went Wrong'
+                ], 500);
+            }
+        }
+
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -10,6 +10,15 @@ class Review extends Model
 {
     use HasFactory;
 
+    protected $table = 'reviews';
+
+    protected $fillable = [
+        'product_id',
+        'customer',
+        'review',
+        'star',
+    ];
+
     public function product(){
         return $this->belongsTo(Product::class);
     }
