d2291400edc353970942a4770da8593d39d38fed
Sohag <80314649+shshohagh@users.noreply.github.com>
2024-07-02 22:36:42 +0600

    add laravel/breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
 import './bootstrap';
+import '../css/app.css';
+
+import { createSSRApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    id: 'my-app',
+    title: (title) => `${title} - ${appName}`,
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+      },
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, App, props, plugin }) {
+        return createSSRApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
